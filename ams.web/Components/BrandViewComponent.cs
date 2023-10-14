5ac164cfd3944ab7d712273638a2f2332dcf7b47
Yunus Ayrigul <yunus@makronet.com>
2023-10-14 23:05:55 +0300

    ams view componet and apartment controller

---
+++ ams.web/Components/BrandViewComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace ams.web.Components
+{
+	using Microsoft.AspNetCore.Mvc;
+
+	public class BrandViewComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return await Task.FromResult(View());
+		}
+	}
+}
---
+++ ams.web/Controllers/ApartmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ams.web.Controllers
+{
+	public class ApartmentController : Controller
+	{
+		public IActionResult Apartments()
+		{
+			return View();
+		}
+	}
+}
