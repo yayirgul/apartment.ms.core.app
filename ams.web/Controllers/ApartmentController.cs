5ab569b674e6cacce02702ca8879d15914d264d7
Yunus Ayirgul <yunusayirgul@hotmail.com>
2024-07-13 00:56:25 +0300

    jquery datatabel revise

--- ams.web/Controllers/ApartmentController.cs
+++ ams.web/Controllers/ApartmentController.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace ams.web.Controllers
+﻿namespace ams.web.Controllers
 {
-	public class ApartmentController : Controller
+    using ams.service.Services.Abstractions;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ApartmentController : Controller
 	{
+        private readonly IApartmentService ApartmentService;
+
+        public ApartmentController(IApartmentService ApartmentService)
+        {
+            this.ApartmentService = ApartmentService;
+        }
+
+        [Route("ams/app/apartments")]
+        public async Task<JsonResult> Apartment()
+        {
+            var r = await ApartmentService.GetAllAsync();
+
+            return Json(r);
+        }
+
         [Route("ams/apartments")]
         public IActionResult Apartments()
 		{
