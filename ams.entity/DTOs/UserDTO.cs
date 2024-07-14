4ae70444c6374517772692b48e5527fa929ac273
Yunus Ayirgul <yunusayirgul@hotmail.com>
2024-07-14 23:10:46 +0300

    header view component

--- ams.entity/DTOs/UserDTO.cs
+++ ams.entity/DTOs/UserDTO.cs
@@ -2,7 +2,19 @@
 {
     public class UserDTO
     {
-        public class LoginDTO
+		public class LoggedDTO 
+		{
+			public Guid Id { get; set; }
+			public string Firstname { get; set; } = null!;
+			public string Lastname { get; set; } = null!;
+			public string Email { get; set; } = null!;
+			public bool EmailConfirmed { get; set; }
+			public string PhoneNumber { get; set; } = null!;
+			public int AccessFailedCount { get; set; }
+			public string Role { get; set; } = null!;
+		}
+
+		public class LoginDTO
         {
             public string Email { get; set; } = null!;
             public string Password { get; set; } = null!;
--- ams.web/Areas/Admin/Components/HeaderViewComponent.cs
+++ ams.web/Areas/Admin/Components/HeaderViewComponent.cs
@@ -1,13 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ams.entity.DTOs;
+using ams.entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ams.web.Areas.Admin.Components
 {
     [Area("admin")]
     public class HeaderViewComponent : ViewComponent
     {
+        private readonly UserManager<AppUser> UserManager;
+        public HeaderViewComponent(UserManager<AppUser> UserManager)
+        {
+            this.UserManager = UserManager;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return await Task.FromResult(View());
+            var logged = await UserManager.GetUserAsync(HttpContext.User); 
+
+            var user = new UserDTO.LoggedDTO
+            {
+                Id = logged!.Id,
+                Firstname = logged!.Firstname!,
+                Lastname = logged!.Lastname!,
+                Email = logged!.Email!,
+				EmailConfirmed = logged!.EmailConfirmed!,
+				PhoneNumber = logged!.PhoneNumber!,
+				AccessFailedCount = logged!.AccessFailedCount!,
+				Role = string.Join("", await UserManager.GetRolesAsync(logged!)),
+            };
+
+			return await Task.FromResult(View(user));
         }
     }
 }
